0f122df1400c272f40126c893887dd249cc5994e
Abhishek Sharma <mrhashcoder@Abhisheks-MacBook-Pro.local>
2024-10-12 16:12:49 +0530

    Added Dev route and Hero Component

---
+++ app/dev/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "@/components/main/hero";
+
+function Development() {
+	return (
+		<div className="p-2 m-2 border border-black">
+			<Hero />
+		</div>
+	);
+}
+
+export default Development;
