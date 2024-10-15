e911da66da26dfe10cfbb9482360d9d7721afdc9
Abhishek Sharma <mrhashcoder@Abhisheks-MacBook-Pro.local>
2024-10-15 17:14:59 +0530

    chore: added afterLogin

--- config/links.ts
+++ config/links.ts
@@ -1,5 +1,29 @@
 import { NavLink } from "@/types"
+import path from "path"
 
-export const privateLinks: NavLink[] = []
+export const privateLinks: NavLink[] = [
+  {
+    name: "root",
+    path: "/",
+    description: "The root",
+  },
+  {
+    name: "dashboard",
+    path: "/dashboard",
+    description: "The Dashboard",
+  },
+  {
+    name: "private",
+    path: "/private",
+    description: "The Private Route",
+  },
+  {
+    name: "invoice",
+    path: "/invoice",
+    description: "the Inoivce",
+  },
+]
 
 export const publicLinks: NavLink[] = []
+
+export const afterLoginLink: NavLink = privateLinks[2]
