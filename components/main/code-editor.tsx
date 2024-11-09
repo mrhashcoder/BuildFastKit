2f5ade263f6f1bc3489a9106e93bec600c294ede
sahitya-chandra <sahityajb@gmail.com>
2024-11-09 13:08:17 +0530

    auto theme change added

--- components/main/code-editor.tsx
+++ components/main/code-editor.tsx
@@ -4,14 +4,19 @@ import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark, vscodeLight } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
+import { useTheme } from "next-themes";
 
 export default function CodeEditor() {
+  const { theme } = useTheme();
+
+  const vscodeTheme = theme === "light" ? vscodeLight : vscodeDark;
+
   return (
     <div className="container mx-auto p-4">
       <CodeMirror
         value="console.log('hello world!');"
         height="200px"
-        theme={vscodeDark}
+        theme={vscodeTheme}
         extensions={[javascript({ jsx: true, typescript: true })]}
         onChange={(value, viewUpdate) => {
           console.log("value:", value);
